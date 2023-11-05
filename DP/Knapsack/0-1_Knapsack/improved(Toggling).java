c80060ba58addee7bb29ba1cb82dc5289b775771
youngrok kim <95115004+rogi-rogi@users.noreply.github.com>
2023-11-06 00:37:26 +0900

    Create improved(Toggling).java

---
+++ DP/Knapsack/0-1_Knapsack/improved(Toggling).java
@@ -0,0 +1,32 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // init
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        // input
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        final int N = Integer.parseInt(st.nextToken());
+        final int K = Integer.parseInt(st.nextToken());
+
+        // solve
+        final int[][] items = new int[N + 1][2]; // (w, v)
+        for (int i = 1; i <= N; ++i) {
+            st = new StringTokenizer(br.readLine());
+            items[i][0] = Integer.parseInt(st.nextToken());
+            items[i][1] = Integer.parseInt(st.nextToken());
+        }
+
+        int[] dp = new int[K + 1];
+        for (int i = 1; i <= N; ++i) {
+            for (int w = K; w >= items[i][0]; --w) {
+                dp[w] = Math.max(dp[w], dp[w - items[i][0]] + items[i][1]);
+            }
+        }
+
+        // output
+        System.out.println(dp[K]);
+    }
+}
