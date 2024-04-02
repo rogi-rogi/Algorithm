447c5211753892822d42145d9084082424f1e983
youngrok kim <95115004+rogi-rogi@users.noreply.github.com>
2024-04-02 13:39:21 +0900

    Create Main.java

---
+++ Fast Start/Main.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        Deque<Character> stack = new ArrayDeque<>();
+
+    }
+}
