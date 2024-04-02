2577f9d2348557d90394ce250a522ecbcadd7bfa
youngrok kim <95115004+rogi-rogi@users.noreply.github.com>
2024-04-02 13:42:10 +0900

    Update Main.java

--- Fast Start/Main.java
+++ Fast Start/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
-        Deque<Character> stack = new ArrayDeque<>();
+        // Deque<> stack = new ArrayDeque<>();
 
     }
 }
