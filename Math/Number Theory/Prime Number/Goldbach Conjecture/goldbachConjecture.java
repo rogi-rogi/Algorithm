2e364cce070f36e00ffd236a30a9babdd5b5c2e0
youngrok kim <95115004+rogi-rogi@users.noreply.github.com>
2024-04-07 16:35:25 +0900

    Update goldbachConjecture.java

--- Math/Number Theory/Prime Number/Goldbach Conjecture/goldbachConjecture.java
+++ Math/Number Theory/Prime Number/Goldbach Conjecture/goldbachConjecture.java
@@ -1,4 +1,5 @@
     private static int goldbachConjecture(ArrayList<Integer> primeList, int num) {
+        if (num % 2 != 0) return -1;
         int cnt = 0;
         for (Integer prime : primeList) {
             if (prime > num / 2) break;
