6121da7df1d971afd1b33fee697890ad298b246a
youngrok kim <95115004+rogi-rogi@users.noreply.github.com>
2024-04-30 23:38:53 +0900

    Create permutation.py

---
+++ Brute-force/Permutation/permutation.py
@@ -0,0 +1,10 @@
+def permutation(arr, visited, cnt):
+    if cnt == N:
+        print(*arr)
+        return
+    for i in range(1, N + 1):
+        if not visited[i]:
+            visited[i] = True
+            arr[cnt] = i
+            permutation(arr, visited, cnt + 1, N)
+            visited[i] = False
